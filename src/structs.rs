fc75f84883fbb0c5fbb5e207a43fa1d1dc5bdfd8
Mayeul Hechinger--Pascot <mayeul.hechinger-pascot@epita.fr>
2024-01-28 21:04:55 +0100

    Add structs and utils modules

---
+++ src/structs.rs
@@ -0,0 +1,158 @@
+multiversx_sc::imports!();
+multiversx_sc::derive_imports!();
+
+#[derive(NestedEncode, NestedDecode, TypeAbi, TopEncode, TopDecode, PartialEq, Debug)]
+pub struct ConfigDTO<T: ManagedTypeApi> {
+    pub genesis_tokenidentifier: TokenIdentifier<T>,
+    pub expansion_tokenidentifier: TokenIdentifier<T>,
+    pub citizen_tokenidentifier: TokenIdentifier<T>,
+    pub max_building: u8,
+    pub max_citizen: u8,
+    pub game_status: bool,
+    pub start_timestamp: u64,
+    pub day: u64,
+    pub episode_number: u64
+}
+
+#[derive(NestedEncode, NestedDecode, TypeAbi, TopEncode, TopDecode, ManagedVecItem, PartialEq, Debug)]
+
+pub struct Move<T: ManagedTypeApi> {
+    pub token_identifier: TokenIdentifier<T>,
+    pub nonce: u64,
+    pub old_bat_pos: u8,
+    pub old_cit_pos: u8,
+    pub new_bat_pos: u8,
+    pub new_cit_pos: u8
+}
+
+#[derive(NestedEncode, NestedDecode, TypeAbi, TopEncode, TopDecode, ManagedVecItem, Clone, PartialEq, Debug)]
+pub struct Citizen<T: ManagedTypeApi> {
+    pub token_identifier: TokenIdentifier<T>,
+    pub nonce: u64,
+}
+
+#[derive(NestedEncode, NestedDecode, TypeAbi, TopEncode, TopDecode, ManagedVecItem, Clone, PartialEq, Debug)]
+pub struct Building<T: ManagedTypeApi> {
+    pub token_identifier: Option<TokenIdentifier<T>>,
+    pub nonce: u64,
+    pub citizens: ManagedVec<T, Option<Citizen<T>>>
+}
+
+#[derive(NestedEncode, NestedDecode, TypeAbi, TopEncode, TopDecode, ManagedVecItem, PartialEq, Debug)]
+pub struct Game<T: ManagedTypeApi> {
+    pub buildings: ManagedVec<T, Building<T>>
+}
+
+#[derive(NestedEncode, NestedDecode, TypeAbi, TopEncode, TopDecode, ManagedVecItem, Debug)]
+pub struct MyEsdtTokenPayment<T: ManagedTypeApi> {
+    pub token_identifier: TokenIdentifier<T>,
+    pub token_nonce: u64,
+    pub amount: BigUint<T>
+}
+
+impl<T: ManagedTypeApi> PartialEq for MyEsdtTokenPayment<T> {
+    fn eq(&self, other: &Self) -> bool {
+        return self.token_identifier == other.token_identifier && self.token_nonce == other.token_nonce && self.amount >= other.amount;
+    }
+
+    fn ne(&self, other: &Self) -> bool {
+        !self.eq(other)
+    }
+}
+
+#[derive(NestedEncode, NestedDecode, TypeAbi, TopEncode, TopDecode, Clone, PartialEq, ManagedVecItem, Debug)]
+pub enum BuildingType {
+    None,
+    Headquarter,
+    Metro,
+    Park,
+    Police_Station,
+    Museum,
+    Hotel,
+    Casino,
+    Restaurant,
+    Airport,
+    Arcade_Room,
+    Stadium,
+    Theme_Park,
+    Rocket_Station,
+    Wine_Bar,
+    TownHall
+}
+
+impl Default for BuildingType {
+    fn default() -> Self {
+        BuildingType::None
+    }
+}
+
+#[derive(NestedEncode, NestedDecode, TypeAbi, TopEncode, TopDecode, Clone, PartialEq, ManagedVecItem, Debug)]
+pub struct Activator<T: ManagedTypeApi> {
+    pub building_types: ManagedVec<T, BuildingType>,
+    pub job_types: ManagedVec<T, ManagedBuffer<T>>,
+    pub citizen_attributes: ManagedVec<T, ManagedBuffer<T>>,
+}
+
+#[derive(NestedEncode, NestedDecode, TypeAbi, TopEncode, TopDecode, Clone, PartialEq, ManagedVecItem, Debug)]
+pub struct Effect<T: ManagedTypeApi> {
+    pub boost_value: BigUint<T>,
+    pub activation_day: BigUint<T>,
+    pub activator: Option<Activator<T>>
+}
+
+#[derive(NestedEncode, NestedDecode, TypeAbi, TopEncode, TopDecode, Clone, PartialEq, ManagedVecItem, Debug)]
+pub struct Event<T: ManagedTypeApi> {
+    pub id: BigUint<T>,
+    pub title: ManagedBuffer<T>,
+    pub description: ManagedBuffer<T>,
+    pub effects: ManagedVec<T, Effect<T>>
+}
+
+impl<T: ManagedTypeApi> Default for Event<T> {
+    fn default() -> Self {
+        Event {
+            id: BigUint::zero(),
+            title: ManagedBuffer::new(),
+            description: ManagedBuffer::new(),
+            effects: ManagedVec::new()
+        }
+    }
+}
+
+
+#[derive(NestedEncode, NestedDecode, TypeAbi, TopEncode, TopDecode, Clone, PartialEq, ManagedVecItem, Debug)]
+pub enum BuildingRarity {
+    None,
+    GenesisClassic,
+    GenesisLegendary,
+    ExpansionClassic,
+    ExpansionRare,
+    ExpansionLegendary,
+    TownHallClassic,
+    TownHallLegendary
+}
+
+#[derive(NestedEncode, NestedDecode, TypeAbi, TopEncode, TopDecode, Clone, PartialEq, ManagedVecItem, Debug)]
+pub struct BuildingAttributes {
+    pub building_type: BuildingType,
+    pub building_rarity: BuildingRarity
+}
+
+#[derive(NestedEncode, NestedDecode, TypeAbi, TopEncode, TopDecode)]
+pub struct CitizenAttributes<T: ManagedTypeApi> {
+    pub id: u64,
+    pub rank: u64,
+    pub rarity_level: u64,
+    pub job: ManagedBuffer<T>,
+    pub building: ManagedBuffer<T>,
+    pub face: ManagedBuffer<T>,
+    pub hair: ManagedBuffer<T>,
+    pub top: ManagedBuffer<T>,
+    pub over_clothe: ManagedBuffer<T>,
+    pub bottom: ManagedBuffer<T>,
+    pub hat: ManagedBuffer<T>,
+    pub held_item: ManagedBuffer<T>,
+    pub eyes_object: ManagedBuffer<T>,
+    pub mouth_object: ManagedBuffer<T>,
+    pub extra: ManagedBuffer<T>,
+}
---
+++ src/utils.rs
@@ -0,0 +1,117 @@
+multiversx_sc::imports!();
+multiversx_sc::derive_imports!();
+
+use crate::structs::{BuildingAttributes, BuildingRarity, BuildingType, CitizenAttributes};
+
+#[elrond_wasm::module]
+pub trait UtilsModule: crate::common::CommonModule
+{
+
+    fn get_genesis_building_attributes(
+        &self,
+        nonce: &u64
+    ) -> BuildingAttributes {
+        let mut building_rarity = match nonce % 2 {
+            1 => BuildingRarity::GenesisClassic,
+            0 => BuildingRarity::GenesisLegendary,
+            _ => unreachable!(),
+        };
+        let building_type = match nonce {
+            1 | 2 => BuildingType::Headquarter,
+            3 | 4 => BuildingType::Metro,
+            5 | 6 => BuildingType::Park,
+            7 | 8 => BuildingType::Police_Station,
+            9 | 10 => BuildingType::Museum,
+            11 | 12 => BuildingType::Hotel,
+            13 | 14 => BuildingType::Casino,
+            15 | 16 => BuildingType::TownHall,
+            _ => BuildingType::None,
+        };
+        if building_type == BuildingType::TownHall {
+            building_rarity = match nonce % 2 {
+                1 => BuildingRarity::TownHallClassic,
+                0 => BuildingRarity::TownHallLegendary,
+                _ => unreachable!(),
+            };
+        }
+        return BuildingAttributes {
+            building_type: building_type,
+            building_rarity: building_rarity
+        };
+    }
+
+    fn get_expansion_building_attributes(
+        &self,
+        nonce: &u64
+    ) -> BuildingAttributes {
+        let building_rarity = match nonce % 3 {
+            1 => BuildingRarity::ExpansionClassic,
+            2 => BuildingRarity::ExpansionRare,
+            0 => BuildingRarity::ExpansionLegendary,
+            _ => unreachable!()
+        };
+        let building_type = match nonce {
+            1 | 2 | 3 => BuildingType::Casino,
+            4 | 5 | 6 => BuildingType::Restaurant,
+            7 | 8 | 9 => BuildingType::Wine_Bar,
+            10 | 11 | 12 => BuildingType::Airport,
+            13 | 14 | 15 => BuildingType::Arcade_Room,
+            16 | 17 | 18 => BuildingType::Stadium,
+            19 | 20 | 21 => BuildingType::Theme_Park,
+            22 | 23 | 24 => BuildingType::Rocket_Station,
+            _ => unreachable!()
+        };
+        return BuildingAttributes {
+            building_type: building_type,
+            building_rarity: building_rarity
+        }
+    }
+
+    fn get_building_attributes(
+        &self,
+        token_identifier: Option<TokenIdentifier>,
+        nonce: &u64
+    ) -> BuildingAttributes
+    {
+        if token_identifier.is_none() {
+            return BuildingAttributes {
+                building_type: BuildingType::None,
+                building_rarity: BuildingRarity::None
+            };
+        }
+        if token_identifier.unwrap() == self.genesis_tokenidentifier().get_token_id() {
+            return self.get_genesis_building_attributes(nonce);
+        } else {
+            return self.get_expansion_building_attributes(nonce);
+        }
+        
+    }
+
+    fn get_citizen_attributes(
+        &self,
+        nonce: u64
+    ) -> CitizenAttributes<Self::Api>
+    {
+        return self.blockchain().get_esdt_token_data(&self.blockchain().get_sc_address(), &self.citizen_tokenidentifier().get_token_id(), nonce).decode_attributes();
+
+    }
+
+    fn get_citizen_traits(
+        &self,
+        attributes: CitizenAttributes<Self::Api>
+    ) -> ManagedVec<ManagedBuffer<Self::Api>>
+    {
+        let mut res: ManagedVec<ManagedBuffer<Self::Api>> = ManagedVec::new();
+        res.push(attributes.face);
+        res.push(attributes.hair);
+        res.push(attributes.top);
+        res.push(attributes.over_clothe);
+        res.push(attributes.bottom);
+        res.push(attributes.hat);
+        res.push(attributes.held_item);
+        res.push(attributes.eyes_object);
+        res.push(attributes.mouth_object);
+        res.push(attributes.extra);
+        return res;
+    }
+}
